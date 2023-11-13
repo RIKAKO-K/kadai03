593697fba1b8e40c90726b79e02a0c80905f200e
RIKAKO <rikako.k.8637@gmail.com>
2023-11-13 21:23:37 +0900

    部分的な変更を追加

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -45,25 +45,23 @@ public class Main {
             //選手登録・変更
             System.out.println();
             Scanner scanner = new Scanner(System.in);
-            System.out.println("-------------選手の更新を行います（新規/変更/削除）-------------");
-            System.out.println("【新規登録の場合：1　変更の場合：2　削除の場合：3】を入力してください");
+            System.out.println("-------------選手の更新を行います（追加/変更/削除）-------------");
+            System.out.println("【追加の場合：1　変更の場合：2　削除の場合：3】を入力してください");
             String EntryCd = scanner.next();
+            System.out.println();
             switch (EntryCd){
                 case "1":
-                    //新規登録
-                    System.out.println();
+                    //追加
                     System.out.println("------選手追加("+ LocalDate.now() + ")------");
                     EntryKbn = eEntryKbn.INSERT;
                     break;
                 case "2":
                     //変更
-                    System.out.println();
                     System.out.println("------選手変更("+ LocalDate.now() + ")------");
                     EntryKbn = eEntryKbn.UPDATE;
                     break;
                 case "3":
                     //削除
-                    System.out.println();
                     System.out.println("------選手削除("+ LocalDate.now() + ")------");
                     EntryKbn = eEntryKbn.DELETE;
                     break;
@@ -73,42 +71,43 @@ public class Main {
             }
 
             //▼背番号入力
-            System.out.println();
-            System.out.println("選手の背番号を入力してください");
-            String number = scanner.next();
+            System.out.println("(追加/変更/削除したい)背番号を入力してください");
+            int number = scanner.nextInt();
 
             //処理前チェック
-            if(samuraiBlue.containsKey(number)){ //★ここで入力した背番号(number)がmapのキーに含んでいるか確認したい
+            if(samuraiBlue.containsKey(number)){
                 //重複あり
                 if(EntryKbn == eEntryKbn.INSERT) {
-                    //新規（重複あり：False)
-                    System.out.println("その背番号は既に使用されています。別の番号を指定してください。");
+                    //新規（重複あり：処理終了)
+                    System.err.println("その背番号は既に使用されています。別の番号を入力してください。");
+                    System.exit(0);
                 }
             }else{
                 //重複なし
                 if(EntryKbn == eEntryKbn.UPDATE || EntryKbn == eEntryKbn.DELETE) {
-                    //変更・削除（重複なし：False）
-                    System.out.println(("該当選手が見つかりません。正しい背番号を再入力してください。"));
+                    //変更・削除（重複なし：処理終了）
+                    System.err.println(("該当選手が見つかりません。正しい背番号を入力してください。"));
+                    System.exit(0);
                 }
             }
-
-            //▼選手名入力
-            System.out.println();
-            System.out.println("名前を入力してください");
-            String name = scanner.next();
 
             //処理実行
             if(EntryKbn == eEntryKbn.DELETE){
                 //削除（remove)
-                samuraiBlue.remove(Integer.valueOf(number));
+                samuraiBlue.remove(number);
             }else{
-                //変更・新規（上書き）
-                samuraiBlue.put(Integer.valueOf(number),name);
+                //▼選手名入力
+                System.out.println();
+                System.out.println("名前を入力してください");
+                String name = scanner.next();
+
+                //新規・変更（上書き）
+                samuraiBlue.put(number,name);
             }
 
             //最終メンバー一覧
             System.out.println();
-            System.out.println("----------SAMURAI BLUE 選手一覧(MF/FW)---------");
+            System.out.println("----------【最終】SAMURAI BLUE 選手一覧(MF/FW)---------");
             samuraiBlue.forEach((key, value) -> System.out.println(key + " : " + value));
 
             //▼次のワールドカップメンバー継続意思入力
@@ -116,12 +115,12 @@ public class Main {
             System.out.println("次のワールドカップもこのメンバーでいきますか？【YES：1　NO：2】");
             String continueKbn = scanner.next();
             if(Objects.equals(continueKbn, "2")){
-                System.out.println("チーム解散");
+                System.out.println("!!!!!!チーム解散!!!!!!");
                 samuraiBlue.clear();
             }
 
         }catch (InputMismatchException e) {
-            //例外発生
+            //例外
             System.out.println("[警告] 入力エラーが発生しました。" + e.getMessage());
         }
     }
